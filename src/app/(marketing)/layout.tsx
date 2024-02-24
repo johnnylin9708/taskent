ee00f4b000e7c861872b50556e2ca65b66565eb0
johnnylin9708 <johnnyjclin@gmail.com>
2024-02-24 13:05:11 +0800

    env set up

---
+++ src/app/(marketing)/layout.tsx
@@ -0,0 +1,5 @@
+const MarketingLayout = ({ children }: { children: React.ReactNode }) => {
+  return <div className="h-full bg-slate-100">{children}</div>;
+};
+
+export default MarketingLayout;
---
+++ src/app/(platform)/(clerk)/layout.tsx
@@ -0,0 +1,5 @@
+const ClerkLayout = ({ children }: { children: React.ReactNode }) => {
+  return <div>{children}</div>;
+};
+
+export default ClerkLayout;
